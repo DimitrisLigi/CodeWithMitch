860a2cecb4c0ae16a234cdfb9912364484ad4cc3
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2021-08-02 20:51:12 +0300

    Adding the Api classes

--- app/src/main/java/api/ApiService.kt
+++ app/src/main/java/api/ApiService.kt
@@ -1,4 +1,10 @@
 package api
 
+import models.UserDomainModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
+    @GET("placeholder/user/{userID}")
+    suspend fun getUser(@Path("userID") userID: String): UserDomainModel
 }
