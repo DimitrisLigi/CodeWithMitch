1e6d1ae7382af1e20d433159acf89cad02da68b1
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2021-08-02 21:06:53 +0300

    Adding the main view model

--- app/src/main/java/viewmodel/MainViewModel.kt
+++ app/src/main/java/viewmodel/MainViewModel.kt
@@ -1,4 +1,27 @@
 package viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import models.UserDomainModel
+import repository.MainRepository
+
+class MainViewModel : ViewModel(){
+
+    private val _userId: MutableLiveData<String> = MutableLiveData()
+
+    val user: LiveData<UserDomainModel> = Transformations.switchMap(_userId){
+        MainRepository.getUser(it)
+    }
+
+
+    fun setUserID(userId: String){
+        if (_userId.value == userId) return
+        _userId.value = userId
+    }
+
+    fun cancelJobs(){
+        MainRepository.cancelJobs()
+    }
 }
