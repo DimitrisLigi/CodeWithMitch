2d48cf5752bbf6d34adccc6942fe501f43921da8
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2021-08-02 20:30:24 +0300

    Adding the domain model

---
+++ app/src/main/java/models/UserDomainModel.kt
@@ -0,0 +1,22 @@
+package models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class UserDomainModel(
+    @Expose
+    @SerializedName("email")
+    val email: String,
+
+    @Expose
+    @SerializedName("username")
+    val username: String,
+
+    @Expose
+    @SerializedName("image")
+    val image: String
+){
+    override fun toString(): String {
+        return "User's email: $email\nUser's username: $username\nUser's image: $image"
+    }
+}
