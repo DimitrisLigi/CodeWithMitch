23350ae62a31e46c8be2a9d4855e1e0e1488e714
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2021-08-02 21:00:24 +0300

    Adding the main repository

--- app/src/main/java/repository/MainRepository.kt
+++ app/src/main/java/repository/MainRepository.kt
@@ -1,4 +1,37 @@
 package repository
 
+import androidx.lifecycle.LiveData
+import api.RetrofitBuilder
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import models.UserDomainModel
+
 object MainRepository {
+
+    //Creating a coroutine Job
+    private var job: CompletableJob? = null
+
+    fun getUser(userID: String): LiveData<UserDomainModel>{
+        job = Job()
+        return object: LiveData<UserDomainModel>(){
+            override fun onActive() {
+                super.onActive()
+                job?.let {
+                    CoroutineScope(IO+it).launch {
+                        val user = RetrofitBuilder.apiService.getUser(userID)
+                        withContext(Main){
+                            value = user
+                            it.complete()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun cancelJobs(){
+        job?.cancel()
+    }
 }
