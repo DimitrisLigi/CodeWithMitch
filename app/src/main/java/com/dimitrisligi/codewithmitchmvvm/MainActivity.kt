795f60ebb5c76eea53c8080ac66edfec21c576b2
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2021-08-02 14:27:07 +0300

    Initial Commit

--- app/src/main/java/com/dimitrisligi/codewithmitchmvvm/MainActivity.kt
+++ app/src/main/java/com/dimitrisligi/codewithmitchmvvm/MainActivity.kt
@@ -2,10 +2,17 @@ package com.dimitrisligi.codewithmitchmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleAdapter
+import androidx.cursoradapter.widget.SimpleCursorAdapter
+import androidx.viewbinding.ViewBinding
+import com.dimitrisligi.codewithmitchmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
